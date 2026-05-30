fcd8eed47a4098f130454d001a3933b0834b2701
renan.silva <renan.silva@sankhya.com.br>
2026-05-30 20:05:28 -0300

    Implementa cálculo de rentabilidade

--- tests_alta/test_app.py
+++ tests_alta/test_app.py
@@ -1,9 +1,12 @@
 import pytest # type: ignore
-from src_alta.app import somar, dividir
+from src_alta.app import calcular_rentabilidade
 
-def test_somar():
-    assert somar(2, 3) == 5
+def test_rentabilidade_50_porcento():
+    assert calcular_rentabilidade(100, 150) == 50
 
-def test_dividir_por_zero():
-    with pytest.raises(ValueError): # type: ignore
-        dividir(10, 0)
+def test_rentabilidade_100_porcento():
+    assert calcular_rentabilidade(100, 200) == 100
+
+def test_custo_zero():
+    with pytest.raises(ValueError):
+        calcular_rentabilidade(0, 100)
