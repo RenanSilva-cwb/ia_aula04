b2a618449b37a15b1d3fc5e0f6f0c90eeadff8c0
renan.silva <renan.silva@sankhya.com.br>
2026-05-30 20:20:48 -0300

    Implementa cobertura baixa

--- tests_baixa/test_app.py
+++ tests_baixa/test_app.py
@@ -4,6 +4,9 @@ from src_alta.app import calcular_rentabilidade, calcular_preco_venda
 def test_rentabilidade_50_porcento():
     assert calcular_rentabilidade(100, 150) == 50
 
+def test_rentabilidade_100_porcento():
+    assert calcular_rentabilidade(100, 200) == 100
+
 def test_markup_50_porcento():
     assert calcular_preco_venda(100, 50) == 150
 
